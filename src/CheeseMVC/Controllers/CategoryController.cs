a363b877cdf97682b520a3eadca9ba26b3409f00
woodwindfreak101 <woodwindfreak101@gmail.com>
2019-01-01 16:51:39 -0600

    Finished first step, about to test migrations

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        
+
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+        public IActionResult Index()
+        {
+            List<CheeseCategory> categories = context.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Add()
+        {
+            AddCategoryViewModel addCategoryViewModel = new AddCategoryViewModel();
+            return View(addCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel addCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add the new cheese to my existing cheeses
+                CheeseCategory newCategory = new CheeseCategory
+                {
+                    Name = addCategoryViewModel.Name,
+                };
+
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+
+                return Redirect("/Category");
+            }
+
+            return View(addCategoryViewModel);
+        }
+    }
+
+}
